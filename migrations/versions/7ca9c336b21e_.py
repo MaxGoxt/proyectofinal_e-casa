1f773190871d81ba25cb9697f1f5b5756d9ab6f5
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-09-15 21:06:26 +0000

    mejora en la navegacion

--- migrations/versions/7ca9c336b21e_.py
+++ migrations/versions/7ca9c336b21e_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: d9f79ebbaa29
+Revision ID: 7ca9c336b21e
 Revises: 
-Create Date: 2023-09-15 17:24:58.556457
+Create Date: 2023-09-15 20:47:52.797290
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd9f79ebbaa29'
+revision = '7ca9c336b21e'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/component/panel_control.jsx
+++ src/front/js/component/panel_control.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import diego from "../../img/diego.jpg";
 import { Context } from '../store/appContext';
@@ -8,12 +8,14 @@ export const PanelCtrl = () => {
     const { store, actions } = useContext(Context)
     return (
         <div className="bg-celeste-claro py-2 fixed-bottom mt-auto text-center azul-oscuro d-flex justify-content-around">
-            <i style={{ fontSize: "40px" }} className="fa-regular fa-heart"></i>
-            {/*<Link to={"/"}>
-                 <button style={{ width: "40px", height: "40px" }} className="btn borde-azul-oscuro border-3 rounded-circle p-auto">
+            <Link to={"/favoritos"}>
+                <i style={{ fontSize: "40px" }} className="fa-regular fa-heart"></i>
+                </Link>
+                <Link to={"/upload"}>
+                <button style={{ width: "40px", height: "40px" }} className="btn borde-azul-oscuro border-3 rounded-circle p-auto">
                     <i className="fa-solid fa-plus"></i>
-                </button> 
-            </Link>*/}
+                </button>
+            </Link>
             <Link to={"/perfil"}>
                 <img src={diego} style={{ width: "40px", height: "40px" }} className="rounded-circle" alt="..." />
             </Link>
