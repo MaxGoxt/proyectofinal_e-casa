4bbc6e762038fa12abcc647a96796a06c1738223
Cristian Díaz <88348311+cristiandiazperdomo@users.noreply.github.com>
2023-09-16 20:48:39 +0000

    modifying main view

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,10 +8,11 @@ export const Navbar = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<nav className="fixed-top navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<img className="ms-2" src={logoecasa} alt="logo ecasa" style={{ width: "30px" }} />
+		<nav className="fixed-top d-block navbar navbar-light bg-light">
+			<div className="d-flex justify-content-between align-items-center" style={{ margin: "0 40px" }}>
+				<Link to="/" className="text-decoration-none text-dark d-flex align-items-end">
+					<img className="mx-2" src={logoecasa} alt="logo ecasa" style={{ width: "40px" }} />
+					<span className="fs-4">E-CASA</span>
 				</Link>
 				{store.auth ?
 					<div className="ml-auto">
