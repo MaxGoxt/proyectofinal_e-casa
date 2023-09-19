458d0b926760499d78e61baa369a32c97b199cb1
Cristian Díaz <88348311+cristiandiazperdomo@users.noreply.github.com>
2023-09-19 18:04:05 -0300

    Update favoritos.jsx

--- src/front/js/pages/favoritos.jsx
+++ src/front/js/pages/favoritos.jsx
@@ -39,23 +39,3 @@ export const Favoritos = () => {
         </div>
     );
 };
-
-
-/*
-    <div className= "mt-5 pt-3">
-        <h1 className= "text-black">Tus favoritos</h1>
-        {store.favoritos == "No tienes favoritos ingresados"? 
-            <h2 className= "text-white bg-azul-oscuro d-flex justify-content-center btn my-4">{store.favoritos}</h2>:
-            <div>
-
-            {store.favoritos.map((item, index) => {
-                    return (
-                        <CardFav key={index} titulo={item.houseId.title} ubicacion={item.houseId.location} id={item.houseId.id} images={item.houseId.images} />
-                    )
-                })}
-            </div>
-            }
-            <PanelCtrl/>
-        </div>
-
- */
