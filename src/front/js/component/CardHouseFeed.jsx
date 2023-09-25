14174405dd712a915dd4a5377e6c00ffcefb8ed6
claudiasgithub2 <138537469+claudiasgithub2@users.noreply.github.com>
2023-09-25 15:53:48 +0000

    funciona y no se rompe

--- src/front/js/component/CardHouseFeed.jsx
+++ src/front/js/component/CardHouseFeed.jsx
@@ -8,19 +8,16 @@ import "../../styles/card_feed_alq.css";
 export const CardHouseFeed = (props) => {
     const { store, actions } = useContext(Context)
 
-    const [isFavorito, setIsFavorito] = useState(store.favoritos.some((item)=> item.houseId.id==props.id));
-//    let isFavorito=store.favoritos.some((item)=> item.houseId.id==props.id)
+    // const [isFavorito, setIsFavorito] = useState(store.favoritos.some((item)=> item.houseId.id==props.id));
+    const [isFavorito, setIsFavorito] = useState(
+        Array.isArray(store.favoritos) && store.favoritos.some((item) => item.houseId.id === props.id)
+      );
 
     useEffect(() => {
         actions.getFavoritos()
 
     }, [])
-
  
-
-    // console.log(store.favoritos["0"]?.houseId.id);
-    // console.log(props.id);
-    // console.log(props.id == store.favoritos["0"]?.houseId.id);
 
     const toggleFavorito = () => {
         setIsFavorito(!isFavorito);
@@ -37,8 +34,17 @@ export const CardHouseFeed = (props) => {
     return (
         <div className="text-decoration-none col-sm-6 col-md-4 col-lg-3 col-xl-2 product"> 
             <div className="thumbnail">
-               
-            <i onClick={()=>toggleFavorito(props.id)} className={store.favoritos?.some((item)=> item.houseId.id==props.id)?"position-absolute fa-solid fa-heart m-2 text-danger bg-dark p-2 bg-opacity-75 rounded" : "position-absolute fa-regular fa-heart m-2 text-danger bg-dark p-2 bg-opacity-75 rounded"} style={{ zIndex: "2", cursor: "pointer" }}></i>
+                <i
+                    onClick={() => toggleFavorito(props.id)}
+                    className={
+                        Array.isArray(store.favoritos) &&
+                            store.favoritos.some((item) => item.houseId.id === props.id)
+                            ? "position-absolute fa-solid fa-heart m-2 text-danger bg-dark p-2 bg-opacity-75 rounded"
+                            : "position-absolute fa-regular fa-heart m-2 text-danger bg-dark p-2 bg-opacity-75 rounded"
+                    }
+                    style={{ zIndex: "2", cursor: "pointer" }}
+                ></i>  
+            {/* <i onClick={()=>toggleFavorito(props.id)} className={store.favoritos?.some((item)=> item.houseId.id==props.id)?"position-absolute fa-solid fa-heart m-2 text-danger bg-dark p-2 bg-opacity-75 rounded" : "position-absolute fa-regular fa-heart m-2 text-danger bg-dark p-2 bg-opacity-75 rounded"} style={{ zIndex: "2", cursor: "pointer" }}></i> */}
                 <div id={"carouselExampleControls" + props.id} className="carousel slide" data-bs-ride="carousel" data-bs-interval="false" data-interval="false" data-mdb-interval="false">
                     <div className="carousel-indicators">
                         <button type="button" data-bs-target={"#carouselExampleControls" + props.id} data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
