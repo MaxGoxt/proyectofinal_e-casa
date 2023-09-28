ef200331d51d2affdf65e31658d9dc99d75da61d
claudiasgithub2 <138537469+claudiasgithub2@users.noreply.github.com>
2023-09-28 14:25:34 +0000

    centrado wifi y estaci

--- src/front/js/pages/edit_prop.jsx
+++ src/front/js/pages/edit_prop.jsx
@@ -45,8 +45,8 @@ export const EditProp = () => {
         let parkingSelected = false;
 
         // CATEGORY
-         alquilerBtn = category.current.childNodes[0].childNodes[0];
-         ventaBtn = category.current.childNodes[1].childNodes[0];
+        alquilerBtn = category.current.childNodes[0].childNodes[0];
+        ventaBtn = category.current.childNodes[1].childNodes[0];
 
         if (alquilerBtn.checked) {
             categorySelected = alquilerBtn.value;
@@ -82,7 +82,7 @@ export const EditProp = () => {
     }
 
     const uploadImage = (id) => {
-        
+
         const {
             categorySelected,
             wifiSelected,
@@ -122,100 +122,100 @@ export const EditProp = () => {
 
     return (
         <div className="d-flex flex-column mt-5 bg-celeste-claro">
-        <h3 className="text-center pt-4">Acá puedes editar tu propiedad</h3>
-            { store.auth ?<>
-            <button className="btn btn-primary mt-5 mx-auto" onClick={() => widgetRef.current.open()}>
-                SUBIR IMAGEN
-            </button>
-            <form onSubmit={(e)=>{e.preventDefault(); uploadImage(param.id); navigate("/mis-propiedades")}} className="d-flex flex-column align-items-center mt-4">
-                <div className="mb-3 w-50">
-                    <label htmlFor="title" className="form-label azul-oscuro fw-bolder">Titulo</label>
-                    <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="title" aria-describedby="emailHelp" ref={title} />
-                </div>
-                <div className="mb-3 w-50">
-                    <label htmlFor="description" className="form-label azul-oscuro fw-bolder">Descripción</label>
-                    <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="description" aria-describedby="emailHelp" ref={description} />
-                </div>
-                <div className="mb-3 w-50 d-flex justify-content-center">
-                    <div className="w-30">
-                        <p className="text-center azul-oscuro fw-bolder">Categoría</p>
-                        <div className="d-flex justify-content-center" ref={category}>
-                            <div className="form-check me-3">
-                                <input className="form-check-input" type="radio" name="category" value="Alquiler" id="flexRadioDefault1" />
-                                <label className="form-check-label" htmlFor="flexRadioDefault1">
-                                    Alquiler
-                                </label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" name="category" value="Venta" id="flexRadioDefault2" />
-                                <label className="form-check-label" htmlFor="flexRadioDefault2">
-                                    Venta
-                                </label>
+            <h3 className="text-center pt-4">Acá puedes editar tu propiedad</h3>
+            {store.auth ? <>
+                <button className="btn btn-primary mt-5 mx-auto" onClick={() => widgetRef.current.open()}>
+                    SUBIR IMAGEN
+                </button>
+                <form onSubmit={(e) => { e.preventDefault(); uploadImage(param.id); navigate("/mis-propiedades") }} className="d-flex flex-column align-items-center mt-4">
+                    <div className="mb-3 w-50">
+                        <label htmlFor="title" className="form-label azul-oscuro fw-bolder">Titulo</label>
+                        <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="title" aria-describedby="emailHelp" ref={title} />
+                    </div>
+                    <div className="mb-3 w-50">
+                        <label htmlFor="description" className="form-label azul-oscuro fw-bolder">Descripción</label>
+                        <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="description" aria-describedby="emailHelp" ref={description} />
+                    </div>
+                    <div className="mb-3 w-50 d-flex justify-content-center">
+                        <div className="w-30">
+                            <p className="text-center azul-oscuro fw-bolder">Categoría</p>
+                            <div className="d-flex justify-content-center" ref={category}>
+                                <div className="form-check me-3">
+                                    <input className="form-check-input" type="radio" name="category" value="Alquiler" id="flexRadioDefault1" />
+                                    <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                        Alquiler
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="radio" name="category" value="Venta" id="flexRadioDefault2" />
+                                    <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                        Venta
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="mb-3 w-50">
-                    <label htmlFor="location" className="form-label azul-oscuro fw-bolder">Ubicación</label>
-                    <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="location" aria-describedby="emailHelp" ref={location} />
-                </div>
-                <div className="d-flex justify-content-around mx-1">
-                <div className="mb-3 w-50 d-flex justify-content-center">
-                    <div className="w-30">
-                        <p className="text-center azul-oscuro fw-bolder">¿Tiene wifi?</p>
-                        <div className="d-flex justify-content-center" ref={wifi}>
-                            <div className="form-check me-3">
-                                <input className="form-check-input" type="radio" value="Si" name="wifi" id="siradio" />
-                                <label className="form-check-label" value="No" htmlFor="siradio">
-                                    <p>Si</p>
-                                </label>
+                    <div className="mb-3 w-50">
+                        <label htmlFor="location" className="form-label azul-oscuro fw-bolder">Ubicación</label>
+                        <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="location" aria-describedby="emailHelp" ref={location} />
+                    </div>
+                    <div className="w-50 d-flex justify-content-evenly mx-1 ">
+                        <div className="mb-3 w-50 d-flex justify-content-around">
+                            <div className="w-30 ">
+                                <p className="text-center azul-oscuro fw-bolder ">¿Tiene wifi?</p>
+                                <div className="d-flex justify-content-center" ref={wifi}>
+                                    <div className="form-check me-3">
+                                        <input className="form-check-input" type="radio" value="Si" name="wifi" id="siradio" />
+                                        <label className="form-check-label" value="No" htmlFor="siradio">
+                                            <p>Si</p>
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" value="No" name="wifi" id="noradio" />
+                                        <label className="form-check-label" htmlFor="noradio">
+                                            No
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" value="No" name="wifi" id="noradio" />
-                                <label className="form-check-label" htmlFor="noradio">
-                                    No
-                                </label>
+                        </div>
+                        <div className="mb-3 w-50 d-flex">
+                            <div className="w-30">
+                                <p className="text-center azul-oscuro fw-bolder">¿Tiene estacionamiento?</p>
+                                <div className="d-flex justify-content-center" ref={parking}>
+                                    <div className="form-check me-3">
+                                        <input className="form-check-input" type="radio" value="Si" name="parking" id="siradio2" />
+                                        <label className="form-check-label" value="No" htmlFor="siradio2">
+                                            Si
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" value="No" name="parking" id="noradio2" />
+                                        <label className="form-check-label" htmlFor="noradio2">
+                                            No
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="mb-3 w-50 d-flex justify-content-center">
-                    <div className="w-30">
-                        <p className="text-center azul-oscuro fw-bolder">¿Tiene estacionamiento?</p>
-                        <div className="d-flex justify-content-center" ref={parking}>
-                            <div className="form-check me-3">
-                                <input className="form-check-input" type="radio" value="Si" name="parking" id="siradio2" />
-                                <label className="form-check-label" value="No" htmlFor="siradio2">
-                                    Si
-                                </label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="radio" value="No" name="parking" id="noradio2" />
-                                <label className="form-check-label" htmlFor="noradio2">
-                                    No
-                                </label>
-                            </div>
-                        </div>
+                    <div className="mb-3 w-50">
+                        <label htmlFor="number_of_rooms" className="form-label azul-oscuro fw-bolder">N° de cuartos</label>
+                        <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="number_of_rooms" aria-describedby="emailHelp" ref={number_of_rooms} />
                     </div>
-                </div>
-                </div>
-                <div className="mb-3 w-50">
-                    <label htmlFor="number_of_rooms" className="form-label azul-oscuro fw-bolder">N° de cuartos</label>
-                    <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="number_of_rooms" aria-describedby="emailHelp" ref={number_of_rooms} />
-                </div>
-                <div className="mb-3 w-50">
-                    <label htmlFor="number_of_bathrooms" className="form-label azul-oscuro fw-bolder">N° de baños</label>
-                    <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="number_of_bathrooms" aria-describedby="emailHelp" ref={number_of_bathrooms} />
-                </div>
-                <div className="mb-3 w-50">
-                    <label htmlFor="price" className="form-label azul-oscuro fw-bolder">Precio</label>
-                    <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="price" aria-describedby="emailHelp" ref={price} />
-                </div>
-                <button type='submit' className="btn btn-primary">Subir casa</button>
-            </form>
-            <div className="d-flex justify-content-center">
-            </div></>
-            : <h2 className='text-danger m-auto mt-5'>🚫INAUTORIZADO🚫</h2>}
+                    <div className="mb-3 w-50">
+                        <label htmlFor="number_of_bathrooms" className="form-label azul-oscuro fw-bolder">N° de baños</label>
+                        <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="number_of_bathrooms" aria-describedby="emailHelp" ref={number_of_bathrooms} />
+                    </div>
+                    <div className="mb-3 w-50">
+                        <label htmlFor="price" className="form-label azul-oscuro fw-bolder">Precio</label>
+                        <input type="text" className="form-control bg-celeste-claro border-bottom border-top-0 border-end-0 border-start-0" id="price" aria-describedby="emailHelp" ref={price} />
+                    </div>
+                    <button type='submit' className="btn btn-primary">Subir casa</button>
+                </form>
+                <div className="d-flex justify-content-center">
+                </div></>
+                : <h2 className='text-danger m-auto mt-5'>🚫INAUTORIZADO🚫</h2>}
         </div>
     );
 }
