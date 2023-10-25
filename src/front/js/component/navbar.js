c86330c9e1dafbd95343742e6e0d076a3dd0d95c
claudiasgithub2 <acsalituro@gmail.com>
2023-10-25 16:08:34 -0300

    alerta con aceptar y cancelar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -11,15 +11,34 @@ export const Navbar = () => {
 
 	const route = useLocation();
 
+
 	useEffect(() => {
 		actions.getPerfil();
 	}, []);
 
-	const mostrarAlerta = () => {
-		window.alert("¡Regístrate para subir tu casa a E-CASA! ¿Deseas registrarte ahora?");
-		console.log("alerta")
 	
+	const mostrarAlerta = () => {
+		const resultado = window.confirm("¡Regístrate para subir tu casa a E-CASA! ¿Deseas registrarte ahora?");
+		if (resultado) {
+		  // El usuario hizo clic en "Aceptar"
+		  window.location.href = "/signup"; // Redirige al formulario de registro
+		} else {
+			window.location.href = "/upload";// El usuario hizo clic en "Cancelar" (se queda en la misma página)
+		}
 	  };
+
+	//   const mostrarAlerta = () => {
+	// 	const resultado = window.confirm("¡Regístrate para subir tu casa a E-CASA! ¿Deseas registrarte ahora?");
+	// 	if (resultado) {
+	// 	  El usuario hizo clic en "Aceptar", no necesitas hacer nada aquí.
+	// 	} else {
+	// 	  El usuario hizo clic en "Cancelar"
+	// 	  if (route.pathname === "/upload") {
+	// 		Solo redirige si no está en la página "/upload" para evitar recargar la página actual.
+	// 		window.location.href = "/upload";
+	// 	  }
+	// 	}
+	//   };
 
 	return (
 		!route.pathname.includes("signup") && !route.pathname.includes("login") && <nav className="fixed-top d-block navbar navbar-light bg-light">
@@ -29,7 +48,7 @@ export const Navbar = () => {
 					<span className="fs-4">E-CASA</span>
 				</Link>
 				<div className="d-flex align-items-center nav-setting-container">
-					<Link to="/upload" className="text-dark fw-bold text-decoration-none me-3 nav_upload_house">
+					<Link to="/signup" className="text-dark fw-bold text-decoration-none me-3 nav_upload_house">
 						<button type="button" className="btn btn-outline-success text-grey" style={{ fontSize: "16px" }} onClick={mostrarAlerta}>Sube tu casa a E-CASA</button>
 					</Link>
 					{store.auth
