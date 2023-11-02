27791bf107c99a9c99bd73ca9edec59cb56dba00
MaxGoxt <m.gomezcoitino@gmail.com>
2023-11-01 21:18:01 -0300

    Carousel en subir casa y arreglo error del cloudinary en useEfect

---
+++ src/front/js/component/Carousel.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useContext } from 'react';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+let imagenes = [
+    "https://res.cloudinary.com/dslz0zdlc/image/upload/v1697546209/bdi1uli8yazfel8dnviq.png",
+    "https://res.cloudinary.com/dslz0zdlc/image/upload/v1697546209/iahed71dsabnjhdmeuzr.png",
+    "https://res.cloudinary.com/dslz0zdlc/image/upload/v1697546243/gqqq8lx98olzexq5q1tl.jpg",
+    "https://res.cloudinary.com/dslz0zdlc/image/upload/v1697546243/gqqq8lx98olzexq5q1tl.jpg",
+    "https://res.cloudinary.com/dslz0zdlc/image/upload/v1697546243/gqqq8lx98olzexq5q1tl.jpg"
+]
+
+export const Carousel = (props) => {
+
+    return (
+        <>
+            <div className="card-header bigger-screen d-flex" style={{ height: "430px" }}>
+          {props.imagesUrl?.map((img, index) => (
+            <img key={index} src={img} className="details-card-img" style={{ maxWidth: "100%", width: "20px", flexGrow: "1", objectFit: "cover", opacity: ".9", transition: ".5s ease" }} />
+          ))}
+        </div>
+        <div id="carouselExampleControls" className="carousel slide mobile-images" data-bs-ride="true">
+          <div className="carousel-inner" >
+            <div className="carousel-inner" style={{ height: "300px" }}>
+              {props.imagesUrl?.map((img, index) => (
+                <div key={index} className={`carousel-item justify-content-center text-center ${index === 0 && "active"}`}>
+                  <img src={img} className="details-card-img" style={{ height: "300px" }} />
+                </div>
+              ))}
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        </>
+    );
+};
+
+Carousel.propTypes = {
+    imagesUrl: PropTypes.array
+}
