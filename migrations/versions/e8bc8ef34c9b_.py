c936a48a9527d920ff51b7ee93ce8e3c79b54067
claudiasgithub2 <acsalituro@gmail.com>
2023-11-16 14:40:21 -0300

    actualizando

--- migrations/versions/e8bc8ef34c9b_.py
+++ migrations/versions/e8bc8ef34c9b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 5fa305fba67a
+Revision ID: e8bc8ef34c9b
 Revises: 
-Create Date: 2023-11-10 17:11:14.238049
+Create Date: 2023-11-14 13:27:01.298951
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5fa305fba67a'
+revision = 'e8bc8ef34c9b'
 down_revision = None
 branch_labels = None
 depends_on = None
