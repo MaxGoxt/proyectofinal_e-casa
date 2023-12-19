a720ab949888545bc98633c7e9a80725765fef0d
Maximiliano Gómez <135187058+MaxGoxt@users.noreply.github.com>
2023-12-19 13:54:11 -0300

    Update 2 render_build.sh
    note: This error originates from a subprocess, and is likely not a problem with pip.
    error: subprocess-exited-with-error
    × Getting requirements to build wheel did not run successfully.
    │ exit code: 1
    ╰─> See above for output.
    note: This error originates from a subprocess, and is likely not a problem with pip.
    [notice] A new release of pip available: 22.2.1 -> 23.3.2
    [notice] To update, run: pip install --upgrade pip

--- render_build.sh
+++ render_build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Instalar requisitos con pipenv
+pipenv install --dev
+
+# Otros comandos de construcción
 npm install
 npm run build
-
-pipenv install
-
 pipenv run upgrade
