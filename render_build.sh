72e2c8be5904cf48deb39535735c6a53393d0a66
Maximiliano Gómez <135187058+MaxGoxt@users.noreply.github.com>
2023-12-19 14:18:08 -0300

    Reset render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -2,10 +2,9 @@
 # exit on error
 set -o errexit
 
-# Instalar requisitos con pipenv
-pipenv install --dev
-
-# Otros comandos de construcción
 npm install
 npm run build
+
+pipenv install
+
 pipenv run upgrade
